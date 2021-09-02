ba0972c991f1a7617930398809b6768783dcb93b
ebrahimghanbari <ghanbari@ualberta.ca>
2021-09-02 12:26:12 -0600

    expressjs running

---
+++ express-back-end/routes/quotes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+router.get("/", (req, res) => {
+  db.select(
+    "q.id as quote_id",
+    "c.first_name as costumer_first_name",
+    "c.last_name as costumer_last_name",
+    "q.destination",
+    "q.price"
+  )
+    .from({ q: "quotes" })
+    .leftJoin({ c: "costumers" }, "q.costumer_id", "c.id")
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+router.get("/:quoteID", (req, res) => {
+  //   res.send(req.params.quoteID);
+  db.select(
+    "q.id as quote_id",
+    "q.from",
+    "q.destination",
+    "q.departure_date",
+    "q.return_date",
+    "q.number_of_traveller",
+    "q.price",
+    "q.quote_status",
+    "c.first_name as costumer_first_name",
+    "c.last_name as costumer_last_name",
+    "c.email as costumer_email",
+    "c.phone as costumer_phone",
+    "c.address as costumer_address",
+    "s.first_name as staff_first_name",
+    "s.last_name as staff_last_name",
+    "s.email as staff_email",
+    "s.phone as staff_phone",
+    "s.address as staff_address"
+  )
+    .from({ q: "quotes" })
+    .leftJoin({ s: "staffs" }, "q.owner_id", "s.id")
+    .leftJoin({ c: "costumers" }, "q.costumer_id", "c.id")
+    .where("q.id", req.params.quoteID)
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+module.exports = router;
---
+++ express-back-end/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+const quotesAPI = require("./routes/quotes");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(helmet());
+app.use("/api/quotes", quotesAPI);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
